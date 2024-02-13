b660a4f70a6e81c05e75d8159327b1b75a14d6a1
IrinaB <balkarovairina@gmail.com>
2024-02-13 10:59:09 +0100

    Refactor Cleanup MapFragment

--- app/src/main/java/ru/irinavb/augmentedhikes/ui/map/MapFragment.kt
+++ app/src/main/java/ru/irinavb/augmentedhikes/ui/map/MapFragment.kt
@@ -1,233 +1,23 @@
 package ru.irinavb.augmentedhikes.ui.map
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.PolylineOptions
 import dagger.hilt.android.AndroidEntryPoint
-import pub.devrel.easypermissions.AppSettingsDialog
-import pub.devrel.easypermissions.EasyPermissions
-import ru.irinavb.augmentedhikes.R
 import ru.irinavb.augmentedhikes.databinding.FragmentMapBinding
-import ru.irinavb.augmentedhikes.services.Polyline
-import ru.irinavb.augmentedhikes.services.TrackingService
-import ru.irinavb.augmentedhikes.utils.Constants.ACTION_PAUSE_SERVICE
-import ru.irinavb.augmentedhikes.utils.Constants.ACTION_START_OR_RESUME_SERVICE
-import ru.irinavb.augmentedhikes.utils.Constants.MAP_ZOOM
-import ru.irinavb.augmentedhikes.utils.Constants.POLYLINE_COLOR
-import ru.irinavb.augmentedhikes.utils.Constants.POLYLINE_WIDTH
-import ru.irinavb.augmentedhikes.utils.Constants.REQUEST_CODE_BACKGROUND_LOCATION_PERMISSION
-import ru.irinavb.augmentedhikes.utils.Constants.REQUEST_CODE_LOCATION_PERMISSION
-import ru.irinavb.augmentedhikes.utils.TrackingUtility
 
 @AndroidEntryPoint
-class MapFragment : Fragment(), EasyPermissions.PermissionCallbacks {
+class MapFragment : Fragment() {
 
-    private var _binding: FragmentMapBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentMapBinding
 
-    private val viewModel: MapViewModel by viewModels()
-
-    private var isTracking = false
-    private var pathPoints = mutableListOf<Polyline>()
-
-
-    private var map: GoogleMap? = null
-
-    @SuppressLint("MissingPermission")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMapBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        binding.mapView.onCreate(savedInstanceState)
-        binding.floatingActionButton.setOnClickListener {
-            toggleRun()
-        }
-        binding.mapView.getMapAsync {
-            map = it
-            addAllPolylines()
-        }
-        subscribeToObservers()
-        return root
-    }
-
-    private fun subscribeToObservers() {
-        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
-            updateTracking(it)
-        })
-
-        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
-            pathPoints = it
-            addLatestPolyline()
-            moveCameraToUser()
-        })
-    }
-
-    private fun toggleRun() {
-        if (isTracking) {
-            sendCommandService(ACTION_PAUSE_SERVICE)
-        } else {
-            sendCommandService(ACTION_START_OR_RESUME_SERVICE)
-        }
-    }
-
-    private fun updateTracking(isTracking: Boolean) {
-        this.isTracking = isTracking
-        if (!isTracking) {
-            binding.floatingActionButton.icon = getDrawable(
-                requireContext(), R.drawable
-                    .ic_play_button
-            )
-        } else {
-            binding.floatingActionButton.icon = getDrawable(
-                requireContext(), R.drawable
-                    .ic_pause_button
-            )
-        }
-    }
-
-    private fun moveCameraToUser() {
-        if (pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
-            map?.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(
-                    pathPoints.last().last(),
-                    MAP_ZOOM
-                )
-            )
-        }
-    }
-
-    private fun addAllPolylines() {
-        for (polyline in pathPoints) {
-            val polylineOptions = PolylineOptions()
-                .color(POLYLINE_COLOR)
-                .width(POLYLINE_WIDTH)
-                .addAll(polyline)
-            map?.addPolyline(polylineOptions)
-        }
-    }
-
-    private fun addLatestPolyline() {
-        if (pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
-            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
-            val lastLatLng = pathPoints.last().last()
-            val polylineOptions = PolylineOptions()
-                .color(POLYLINE_COLOR)
-                .width(POLYLINE_WIDTH)
-                .add(preLastLatLng)
-                .add(lastLatLng)
-            map?.addPolyline(polylineOptions)
-        }
-    }
-
-    private fun sendCommandService(action: String) {
-        Intent(requireContext(), TrackingService::class.java).also {
-            it.action = action
-            requireContext().startService(it)
-            requireContext()
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        requestPermissions()
-    }
-
-    private fun requestPermissions() {
-        if (TrackingUtility.hasLocationPermissions(requireContext()) && TrackingUtility
-                .hasBackgroundLocationPermissions(requireContext())
-        ) {
-            return
-        }
-
-        if (!TrackingUtility.hasLocationPermissions(requireContext())) {
-            EasyPermissions.requestPermissions(
-                this,
-                "You need to accept location permissions to use this app",
-                REQUEST_CODE_LOCATION_PERMISSION,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        }
-
-        if (!TrackingUtility.hasBackgroundLocationPermissions(requireContext()) &&
-            Build.VERSION.SDK_INT > Build.VERSION_CODES.Q
-        ) {
-            EasyPermissions.requestPermissions(
-                this,
-                "You need to accept background location to use this app",
-                REQUEST_CODE_BACKGROUND_LOCATION_PERMISSION,
-                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
-            )
-        }
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
-
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            AppSettingsDialog.Builder(this).build().show()
-        } else {
-            requestPermissions()
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.mapView.onResume()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        binding.mapView.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        binding.mapView.onStop()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        binding.mapView.onPause()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        binding.mapView.onLowMemory()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        binding.mapView.onSaveInstanceState(outState)
+        binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
